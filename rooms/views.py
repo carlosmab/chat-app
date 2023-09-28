8b927c01e593497fb20504033cd58f5a9cc04e18
carlosmab <carlosm.araujob@gmail.com>
2023-09-28 10:02:45 -0500

    Implemented room detailed view

--- rooms/views.py
+++ rooms/views.py
@@ -7,3 +7,9 @@ from .models import Room
 def rooms(request):
     rooms = Room.objects.all()
     return render(request, 'rooms/rooms.html', {'rooms': rooms})
+
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    return render(request, 'rooms/room.html', {'room': room})
