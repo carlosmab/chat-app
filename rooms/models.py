89315d997fd39d055a9ae10aa56840b47fd50c55
carlosmab <carlosm.araujob@gmail.com>
2023-09-28 11:47:42 -0500

    Sending messages

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    
+    
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
     
